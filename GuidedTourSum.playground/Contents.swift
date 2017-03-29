0893af34b03b227398f0f3f55b6fe1788fadc5ff
vmachiel <vmachiel@gmail.com>
2017-03-29 19:36:29 +0200

    Small updates

--- GuidedTourSum.playground/Contents.swift
+++ GuidedTourSum.playground/Contents.swift
@@ -5,9 +5,9 @@ print("Hello World")
 /* Variables are declared once with "var" and constants with "let
  When you assign a new value to a variable it needs to be of the same type */
 
-
 var myVariable = 42
 myVariable = 51
 print(myVariable)
 let myConstant = 51.2
 
+/* */
