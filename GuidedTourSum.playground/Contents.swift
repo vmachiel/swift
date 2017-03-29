da8e78b33641798f0ffc20f03a6738f77270cb9f
vmachiel <vmachiel@gmail.com>
2017-03-29 17:24:52 +0200

    Started learning swift!

--- GuidedTourSum.playground/Contents.swift
+++ GuidedTourSum.playground/Contents.swift
@@ -1,5 +1,13 @@
-//: Playground - noun: a place where people can play
+// Hello world is easy, complete with autonewline!
 
-import UIKit
+print("Hello World")
 
-var str = "Hello, playground"
+/* Variables are declared once with "var" and constants with "let
+ When you assign a new value to a variable it needs to be of the same type */
+
+
+var myVariable = 42
+myVariable = 51
+print(myVariable)
+let myConstant = 51.2
+
