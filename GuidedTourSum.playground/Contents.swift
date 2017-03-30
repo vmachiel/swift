efcbbf704e787f46860ab4c7e7b2dd42f5b83b79
vmachiel <vmachiel@gmail.com>
2017-03-30 16:36:09 +0200

    Minor changes, nothing noteworthy.

--- GuidedTourSum.playground/Contents.swift
+++ GuidedTourSum.playground/Contents.swift
@@ -10,4 +10,16 @@ myVariable = 51
 print(myVariable)
 let myConstant = 51.2
 
-/* */
+/* Compiler can infer the type at initialization. You can also 
+ explicitely define it. You must also define conversion to another
+ type if needed! */
+
+let implicitDouble = 23.4
+let explicitDouble: Double = 22
+let explicitFloat: Float = 53
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
+// You can 
