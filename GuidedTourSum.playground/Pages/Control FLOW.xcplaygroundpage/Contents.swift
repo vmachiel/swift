bfc3925b80904778970214dfa6e8aed95dee162d
vmachiel <vmachiel@gmail.com>
2017-04-04 19:40:20 +0200

    Progress. It's small, but it's whenever I have time :)

--- GuidedTourSum.playground/Pages/Control FLOW.xcplaygroundpage/Contents.swift
+++ GuidedTourSum.playground/Pages/Control FLOW.xcplaygroundpage/Contents.swift
@@ -11,6 +11,23 @@ for score in individualScores {
 }
 print(teamScore)
 
+// You can also use the for-in combo to loop over dictionaries just like python for k,v in dict
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25]
+]
+var largest = 0
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+print(largest)
+
 // You can also use optionals: values that are either "there" or nil. 
 // State in the declaration if the variable is optional.
 var optionalString: String? = "Hello"
@@ -29,3 +46,30 @@ if let greeting = optionalString{
 
 // You can also provide a default using ?? If the optional value is missing
 // the default one is used.
+let nickName: String? = nil
+let fullName: String = "Machiel van Dorst"
+let informalGreeting = "Hi \(nickName ?? fullName)" // nickName is empty so fullName is used
+
+// Switch statements aren't bound to one variable type, or true/false, ints only etc. 
+// They need to be exhaustive: include a default case! If a match is found, the code after 
+// the : is executed and the program exits the switch block.
+
+let vegetable = "red pepper"
+
+switch vegetable {  // let's look at vegetable
+
+case "celery":  // is it "celery"?
+    print("Add some raisins and make ants on a log.")
+case "cucumber", "watercress":  // is it a list of "cucumber", "watercress"
+    print("That would make a good tea sandwich.")
+// This one is interesting: if vegetable ends in pepper (it does), "red pepper" is assigened to local
+// variable x and can be used in the following statement
+case let x where x.hasSuffix("pepper"):
+    print("Is it a spicy \(x)?")
+default:
+    print("Everything tastes good in soup.")
+}
+
+
+
+
