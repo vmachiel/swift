d2e5ae2a1e66885d748ec46005453edcafd70ef4
vmachiel <vmachiel@gmail.com>
2017-07-24 19:33:11 +0200

    Created a new project to build a calculator

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Machiel van Dorst on 24-07-17.
+//  Copyright © 2017 vmachiel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
