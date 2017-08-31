d7a880dec23f3d7b151a1ef5728c323e4bdd7eb5
vmachiel <vmachiel@gmail.com>
2017-08-31 18:06:40 +0200

    Setting up an example of delegates/protocols communication pattern.

---
+++ DelegatesExample/DelegatesExample/BaseScreen.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  DelegatesExample
+//
+//  Created by Machiel van Dorst on 28-08-17.
+//  Copyright © 2017 vmachiel. All rights reserved.
+//
+
+import UIKit
+
+// This is the "Intern" view: it gets told what to do.
+
+class BaseScreen: UIViewController {
+
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var chooseButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        chooseButton.layer.cornerRadius = chooseButton.frame.size.height / 2   // round corners
+    }
+
+    @IBAction func chooseButton(_ sender: UIButton) {
+        let selectionVC = storyboard?.instantiateViewController(withIdentifier: "SelectionScreen") as! SelectionScreen
+        present(selectionVC, animated: true, completion: nil)
+        
+    }
+}
+
