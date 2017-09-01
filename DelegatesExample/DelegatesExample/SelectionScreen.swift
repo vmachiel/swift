ee474c338d019250957f4b4728d270981246d754
vmachiel <vmachiel@gmail.com>
2017-09-01 11:25:08 +0200

    Done with delegates protocols example, on to observer/notification

--- DelegatesExample/DelegatesExample/SelectionScreen.swift
+++ DelegatesExample/DelegatesExample/SelectionScreen.swift
@@ -18,8 +18,8 @@ protocol SideSelectionDelegate {
 
 class SelectionScreen: UIViewController {
     
-    // This is actually an instance of the "intern". Give it some orders.
-    
+    // This is actually an instance of the "intern". Which can be used to give it some orders. 
+    // The BaseScreen (intern) will set itself as a delegate of SelectionScreen (boss) 
     var selectionDelegate: SideSelectionDelegate!
 
     override func viewDidLoad() {
@@ -30,12 +30,12 @@ class SelectionScreen: UIViewController {
 
     // You can force unwrap the images since we know we have them! Don't do this with network images!
     @IBAction func rebelButtonTapped(_ sender: UIButton) {
-        selectionDelegate.didTapChoice(image: UIImage(named: "vader")!, name: "Darth Vader", color: .white)
+        selectionDelegate.didTapChoice(image: UIImage(named: "vader")!, name: "Darth Vader", color: .red)
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func imperialButtonTapped(_ sender: UIButton) {
-        selectionDelegate.didTapChoice(image: UIImage(named: "luke")!, name: "Luke Skywalker", color: .red)
+        selectionDelegate.didTapChoice(image: UIImage(named: "luke")!, name: "Luke Skywalker", color: .cyan)
         dismiss(animated: true, completion: nil)
     }
 
