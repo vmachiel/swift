fc3c1af8525f8538baf1bf71c1daf9a2ce21df35
vmachiel <vmachiel@gmail.com>
2017-09-02 15:47:46 +0200

    It works! Add documentation later

--- ObserversExample/ObserversExample/BaseScreen.swift
+++ ObserversExample/ObserversExample/BaseScreen.swift
@@ -8,15 +8,82 @@
 
 import UIKit
 
+// THIS IS THE OBSERVER, Which can listen to many different notification senders. 
+
+// The keys for the different notification. The observer (Base) can react to each differently.
+// You declare these globally. Normally, create a constants file to declare global variables. This ex. is simple
+// They have to be unique, use your bundleID to achieve that (not necessary, good practice though)
+let lightNotificationKey = "com.vmachiel.lightSide"
+let darkNotificationKey = "com.vmachiel.darkSide"
+// Note that it's best practice to remove observers when they are no longer necessary. Do this in this case
+// when the BaseScreen is de-allocated.
+
 class BaseScreen: UIViewController {
 
     @IBOutlet weak var mainImageView: UIImageView!
     @IBOutlet weak var chooseButton: UIButton!
     @IBOutlet weak var nameLabel: UILabel!
     
+    // Create variables to reference each notification object. You also set these in the SelectionScreen to
+    // be sent
+    let light = Notification.Name(rawValue: lightNotificationKey)
+    let dark = Notification.Name(rawValue: darkNotificationKey)
+    
+    // Remove the observer when the screen is deinited. This is best practice, otherwise you'll have 
+    // obserers all over the place in big projects, listening when it's not necessary.
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         chooseButton.layer.cornerRadius = chooseButton.frame.size.height / 2   // round corners
+        createObservers()
+    }
+    
+    // Create the observers in seperate function. This method sets them up, and is called from viewDidLoad()
+    // Some are redundant, but their purpose here is to demo Observers
+    func createObservers() {
+        
+        // light side observers
+        // So add observers. So the init with 4 arguments: 1: what is the observer? In this case, this view is
+        // so self. This is the object "registering as an observer"
+        // 2: selector: what method is called when the notification is received.
+        // 3: name: what obsever to respond to. This name is send from the SelectionScreen.
+        // 4: What object to pass to the selector, nil in this case.
+        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateCharacterImage(notification:)), name: light, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateNameLabel(notification:)), name: light, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateBackground(notification:)), name: light, object: nil)
+        
+        //Dark side observers.
+        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateCharacterImage(notification:)), name: dark, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateNameLabel(notification:)), name: dark, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateBackground(notification:)), name: dark, object: nil)
+    }
+    
+    // The methods that get called when the observer (self) gets the messages (notifications)
+    // Each one checks if the name is light, and sets the image/name/background to light side, OR dark
+    // and sets everything 
+    func updateCharacterImage(notification: NSNotification) {
+        
+        let isLight = notification.name == light
+        let image = isLight ? UIImage(named: "luke")! : UIImage(named: "vader")!
+        mainImageView.image = image
+    }
+    
+    func updateNameLabel(notification: NSNotification) {
+        
+        let isLight = notification.name == light
+        let name = isLight ? "Luke Skywalker" : "Darth Vader"
+        nameLabel.text = name
+        
+    }
+    
+    func updateBackground(notification: NSNotification) {
+        
+        let isLight = notification.name == light
+        let color = isLight ? UIColor.cyan : UIColor.red
+        view.backgroundColor = color
     }
 
     @IBAction func chooseButton(_ sender: UIButton) {
@@ -25,6 +92,14 @@ class BaseScreen: UIViewController {
         
     }
 }
+
+
+
+
+
+
+
+
 
 
 
--- ObserversExample/ObserversExample/SelectionScreen.swift
+++ ObserversExample/ObserversExample/SelectionScreen.swift
@@ -8,17 +8,28 @@
 
 import UIKit
 
+// This is a sender of noticiations. Send different ones dependent on what button is pressed.
+
 class SelectionScreen: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    // So define a name for a noticiation (rawvalue of an enum). Use the global notification strings
+    // defined in the observer (or globals.swift). Seperate constant for redability.
+    // Then post that using default.post. Send the name, and object = nil since no objects need
+    // to be passed here. Just the notification needs to go out
+    // Finally, dismiss this view.
     @IBAction func rebelButtonTapped(_ sender: UIButton) {
+        let name = Notification.Name(rawValue: darkNotificationKey)
+        NotificationCenter.default.post(name: name, object: nil)
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func imperialButtonTapped(_ sender: UIButton) {
+        let name = Notification.Name(rawValue: lightNotificationKey)
+        NotificationCenter.default.post(name: name, object: nil)
         dismiss(animated: true, completion: nil)
     }
 
