b26bf6ebbbb86eb86c057fc39c840970b3ea1d52
vmachiel <vmachiel@gmail.com>
2018-04-02 13:28:13 +0200

    Swift 4.1

---
+++ Concentration/Concentration/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Machiel van Dorst on 25-03-18.
+//  Copyright © 2018 vmachiel. All rights reserved.
+//
+
+import Foundation
+
+
+// Part of model: defines that card.
+
+struct Card {
+    
+    
+    
+}
---
+++ Concentration/Concentration/Concentration.swift
@@ -0,0 +1,24 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Machiel van Dorst on 24-03-18.
+//  Copyright © 2018 vmachiel. All rights reserved.
+//
+
+import Foundation
+
+// The brain of the concentration game. Part of model in MVC. Name the file after the main class
+// Think about what your public API should be like: get into the public/private design.
+
+class Concentration {
+    
+    // public the cards that are availible
+    var cards: Array<Card>
+    
+    // Main public function: the method that gets called when a user picks a card.
+    func chooseCard(at index: Int) {
+        
+    }
+    
+}
