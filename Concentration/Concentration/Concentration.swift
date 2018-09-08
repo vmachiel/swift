8538a35746df698de4823c9ae5f27abf1d074a41
vmachiel <vmachiel@gmail.com>
2018-09-08 15:40:39 +0200

    UI working, now to make it play the game

--- Concentration/Concentration/Concentration.swift
+++ Concentration/Concentration/Concentration.swift
@@ -18,7 +18,8 @@ class Concentration {
     var cards = [Card]()
     
     // Init for the Concentration class. Make a card with an ID and add two of them to cards array.
-    // Do this for the number of pairs you want.
+    // Do this for the number of pairs you want. This will create two identical cards (same emoji in the
+    // view, same identifier here). 
     init(numberOfPairsOfCards: Int) {
         for _ in 0..<numberOfPairsOfCards {
             let card = Card()   // The unique ID is made here! See Card.swift
@@ -29,8 +30,13 @@ class Concentration {
     }
     // MARK: Methods
     // Main public function: the method that gets called when a user picks a card.
+    // For now, just flip the card
     func chooseCard(at index: Int) {
-        
+        if cards[index].isFaceUp {
+            cards[index].isFaceUp = false
+        } else {
+            cards[index].isFaceUp = true
+        }
     }    
 }
 
--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+// KEEP IN MIND: the difference between index in cardButtons, and identifier of a Card.
 class ViewController: UIViewController {
     // MARK: Outlets and properties
     // The Label that displays the flip count
@@ -28,13 +29,14 @@ class ViewController: UIViewController {
     }
     // All possible emojis.
     var emojiChoices = ["🦇", "😱", "🙀", "😈", "🎃", "👻", "🍭", "🍬", "🍎"]
+    // Dictionaries that hold the match between the IDENTIFIER OF THE CARD, and the emoji
+    // identifier can be use because two cards with same ID need to be the samen.
+    var emoji = [Int: String]()
     
     // MARK: Interface Actions
     // Method that's called when a card is touched
     // It takes the index of the button you pressed (cardButtons) and passes it to the model's
     // to the model's method chooseCard (using the var game). Update view
-    // Keep in mind: the index used here is the index of the card in the cardsButtons array. This is
-    // NOT the ID of the pair of cards used in Concentration/Card (or is it.. check)
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
         if let cardNumber = cardButtons.index(of: sender) {
@@ -44,16 +46,24 @@ class ViewController: UIViewController {
     }
     // Mark: VC Methods
     // Looks at the current state of the cards (model) and updates the view
+    // Call everytime a button from the array cardButtons is touched by the user.
+    // For each card button in the array, get their view state and model state
+    // Check what the model state is, and update the view accordingly
+    // !! You use index of cardButtons for the button/view of an object in the view. And
+    // you use that index to get a card object stored in the instance of the Constration model
+    // (see Concentration.swift). Those will match because of how the model is inited. Each card
+    // will have an identifier, equal to one other card)
     func updateViewFromModel() {
         for index in cardButtons.indices {
             let button = cardButtons[index]  // View, index in cardButtons
             let card = game.cards[index]  // Model, index in cards in the Concentration model through game instance
-            // Matches the button with the card
-            // If card is faceup, call the emojie method to set  and make it with
+            // So if the model of this particular card is now faceup, set emoji and turn white
+            // The emoji method is based on card.identifier, since it is the same for the pair.
             if card.isFaceUp {
                 button.setTitle(emoji(for: card), for: UIControlState.normal)
                 button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
             // If card is facedown, make it orang with no title, or transparant if already matched.
+            // matching and done are not implemented yet. 
             } else {
                 button.setTitle("", for: UIControlState.normal)
                 button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)  // If matched, make it clear so you don't see
@@ -61,9 +71,16 @@ class ViewController: UIViewController {
             }
         }
     }
-    
+    // Returns the emoji a particular Card.identifier. It first checks if the card id
+    // has an entry in the dict that links Card.identifier with an emoji. If not,
+    // it takes a random emoji out of the array, associates it with the Card.identifier and removes
+    // it from the array. It only works if there are still emojis in the emojiChoices array.
     func emoji(for card: Card) -> String {
-        return "?"
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)  // returns the value it removed
+        }
+        return emoji[card.identifier] ?? "?"
     }
 }
 
