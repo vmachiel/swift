718eed7962514e5dd1b88249ae5d2f4d23f58ff9
vmachiel <vmachiel@gmail.com>
2021-10-31 11:25:35 +0100

    Small update

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Machiel van Dorst on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
